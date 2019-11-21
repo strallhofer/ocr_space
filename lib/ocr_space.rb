516ad660a2b06d0cb678d7421e23cb0206e2f35b
Elmar Strallhofer <elmar.strallhofer@strallhofer.com>
2019-11-21 10:56:27 +0100

    improve error handling
    If an error occurs, return value is always nil, and erros are stored in @errors.
    Also a "remove" option is added to clean_convert, specifying if LF, CR etc. should be deleted. Multi-page texts are now also possible with clean_convert.

--- lib/ocr_space.rb
+++ lib/ocr_space.rb
@@ -4,13 +4,14 @@ require 'ocr_space/banner'
 
 module OcrSpace
   class Resource
-    attr_reader :data
+    attr_reader :data, :errors
 
     include OcrSpace::Convert
 
     def initialize(apikey: ENV['ocr_api_key'])
       @api_key = apikey
       @data = nil
+      @errors = []
     end
   end
 end
--- lib/ocr_space/convert.rb
+++ lib/ocr_space/convert.rb
@@ -1,46 +1,51 @@
 require 'ocr_space/file_post'
 module OcrSpace
-    module Convert
-        def convert(apikey: @api_key, language: 'eng', isOverlayRequired: false, file: nil, url: nil)
-          if file
-            @files = File.new(file)
-            @data = OcrSpace::FilePost.post('/parse/image',
-                                            body: { apikey: apikey,
-                                                    language: language,
-                                                    isOverlayRequired: isOverlayRequired,
-                                                    file: @files })
-            @data = @data.parsed_response['ParsedResults']
-          elsif url
-            @data = HTTParty.post('https://api.ocr.space/parse/image',
-                                  body: { apikey: apikey,
-                                          language: language,
-                                          isOverlayRequired: isOverlayRequired,
-                                          url: url })
-            @data = @data.parsed_response['ParsedResults']
-          else
-            "You need to Pass either file or url."
-          end
-        end
+  module Convert
+    def convert(apikey: @api_key, language: 'eng', filetype: nil, isOverlayRequired: false,
+      file: nil, url: nil, scale: true, isTable: true)
 
-        def clean_convert(apikey: @api_key, language: 'eng', isOverlayRequired: false, file: nil, url: nil)
+      unless file || url
+        @errors << "You need to Pass either file or url."
+        return nil
+      end
+
+      body = { apikey: apikey,
+        language: language,
+        filetype: filetype,
+        isOverlayRequired: isOverlayRequired,
+        scale: scale,
+        isTable: isTable}
+
+        begin
           if file
-            @files = File.new(file)
-            @data = OcrSpace::FilePost.post('/parse/image',
-                                            body: { apikey: apikey,
-                                                    language: language,
-                                                    isOverlayRequired: isOverlayRequired,
-                                                    file: @files })
-            @data = @data.parsed_response['ParsedResults'][0]["ParsedText"].gsub(/\r|\n/, "")
+            body[:file] = File.new(file)
+            @data = OcrSpace::FilePost.post('/parse/image', body: body)
           elsif url
-            @data = HTTParty.post('https://api.ocr.space/parse/image',
-                                  body: { apikey: apikey,
-                                          language: language,
-                                          isOverlayRequired: isOverlayRequired,
-                                          url: url })
-            @data = @data.parsed_response['ParsedResults'][0]["ParsedText"].gsub(/\r|\n/, "")
-          else
-            "You need to Pass either file or url."
+            body[:url] = url
+            @data = HTTParty.post('https://api.ocr.space/parse/image', body: body)
           end
+        rescue Exception => e
+          @errors << e
+          return
         end
+        
+        if not @data
+          @errors << "No response received."
+          nil
+        elsif not @data.parsed_response['ParsedResults']
+          @errors += @data.parsed_response['ErrorMessage']
+          nil
+        else
+          @data = @data.parsed_response['ParsedResults']
+        end
+      end
+
+      def clean_convert(apikey: @api_key, language: 'eng', filetype: nil, isOverlayRequired: false,
+        file: nil, url: nil, scale: true, isTable: true, remove: /\r|\n/)
+        @data = convert(apikey: apikey, language: language, filetype: filetype, isOverlayRequired: isOverlayRequired, file: file, url: url)
+
+        # Read text per page and join pages
+        @data = @data && @data.map{|data|data["ParsedText"]}.join("\r\n").gsub(remove, "")
+      end
     end
-end
+  end
